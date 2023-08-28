d06e147a0aa13fbefa81e887452b541d12293a91
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2023-08-29 01:02:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+
+// Create a function that returns a promise which resolves with a string Hello, world! after 1 second. Use this promise to update the text of an HTML element with an ID of output after it resolves.
+async function callMe(){
+    let promise = new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Hello, World");
+        }, 1000)
+    })
+    return promise;
+}
+
+callMe().then((data)=>{
+    output.innerText = data;
+})
